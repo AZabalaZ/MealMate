7e1a7b2a8aa52965925b1a6528778dcddcd68002
fernanda vallejos <fernanda.vallejos@uc.cl>
2024-03-05 15:50:19 -0300

    mostrar-comida

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_04_182444) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_150402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_04_182444) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ingredients", default: [], array: true
+    t.string "image_url"
   end
 
   create_table "recipes", force: :cascade do |t|
